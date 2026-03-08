27689cd9c60c753ce27e451d99cf1dd29683f001
WILKER OFC <developer.wilker.ofc@gmail.com>
2026-03-08 14:12:46 -0400

    Actualizar eco_minar.js

--- plugins/eco_minar.js
+++ plugins/eco_minar.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn }) => {
   let coal = pickRandom([500, 600, 700, 800, 1000, 1200, 1500, 2000])
   let stone = pickRandom([2000, 3000, 4000, 5000, 6000, 7000, 8000, 10000])
 
-  let img = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745557957843.jpeg' // Cambia esta imagen por una de Gohan Bestia si quieres
+  let img = 'https://d.uguu.se/JwljtGzp.jpg' // Cambia esta imagen por una de Gohan Bestia si quieres
   let cooldownTime = 600000
   let now = Date.now()
   let nextMine = (user.lastmiming || 0) + cooldownTime
