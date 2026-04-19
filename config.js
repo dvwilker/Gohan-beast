280cb54c3d522c82edc57171ea9b09793ece08b4
The King wilker <sandovalwilkerson700@gmail.com>
2026-04-18 23:09:56 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -25,13 +25,13 @@ global.jadi = 'subsaiyan'
 global.yukiJadibts = true
 
 global.namecanal = '❇️'
-global.idcanal = '120363403739366547@newsletter'
-global.idcanal2 = '120363403739366547@newsletter'
-global.canal = 'https://whatsapp.com/channel/0029Vb5pM031CYoMvQi2I02D'
-global.canalreg = '120363402895449162@newsletter'
+global.idcanal = '120363404707199986@newsletter'
+global.idcanal2 = '120363404707199986@newsletter'
+global.canal = 'https://whatsapp.com/channel/0029Vb7ntULLY6d8uOvyDy0C'
+global.canalreg = '120363404707199986@newsletter'
 
 global.ch = {
-  ch1: '120363420941524030@newsletter'
+  ch1: '120363404707199986@newsletter'
 }
 
 global.multiplier = 69
